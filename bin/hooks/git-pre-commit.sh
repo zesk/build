da998fc2eae13504a6fcdbd922c5e7544b7ae983
Kent Davidson <kent@marketacumen.com>
2023-10-15 22:13:02 -0400

    updating pc and adding copy

--- bin/hooks/git-pre-commit.sh
+++ bin/hooks/git-pre-commit.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
+#
+# cp ./bin/hooks/git-pre-commit.sh .git/hooks/pre-commit
+#
+# Part of build system integration with git
+#
+# Copyright &copy; 2023 Market Acumen, Inc.
+#
 
 set -eo pipefail
 
 cd "$(dirname "${BASH_SOURCE[0]}")/../.."
 
 fromTo=(bin/hooks/git-pre-commit.sh .git/hooks/pre-commit)
-if ! diff -q "${fromTo[@]}"; then
+if ! diff -q "${fromTo[@]}" >/dev/null; then
+    echo -n "Git pre-commit hook was updated ..."
     cp "${fromTo[@]}"
     exec .git/hooks/pre-commit "$@"
 fi
@@ -13,12 +21,15 @@ fi
 # shellcheck source=/dev/null
 . ./bin/build/tools.sh
 
-./bin/build/chmod-sh.sh
-./bin/update-md.sh
+statusMessage consoleSuccess Making shell files executable ...
+./bin/build/chmod-sh.sh >/dev/null
+statusMessage consoleSuccess Updating help files ...
+./bin/update-md.sh >/dev/null
 if which shellcheck >/dev/null; then
     failedReasons=()
     thisYear=$(date +%Y)
 
+    statusMessage consoleSuccess Running shellcheck ...
     while IFS= read -r -d '' f; do
         if ! shellcheck "$f" >/dev/null; then
             failedReasons+=("shellcheck $f failed")
