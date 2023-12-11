95cdfdafb91e9d7919ef9332d454acfc2549e030
Kent Davidson <kent@marketacumen.com>
2023-12-11 01:23:47 -0500

    adding tool

--- bin/_labs/ssh-setup.sh
+++ bin/_labs/ssh-setup.sh
@@ -16,98 +16,106 @@ errorArgument=2
 # Create a key for a user for SSH authentication to other servers.
 #
 sshSetup() {
-	local sshHomePath flagForce servers keyType keyBits
+  local sshHomePath flagForce servers keyType keyBits
 
-	if [ ! -d "${HOME-}" ]; then
-		consoleError "$errorArgument" "HOME is not defined and is required"
-	fi
-	sshHomePath="$HOME/.ssh/"
-	flagForce=0
-	servers=
-	keyType=ed25519
-	keyBits=2048
+  if [ ! -d "${HOME-}" ]; then
+    consoleError "$errorArgument" "HOME is not defined and is required"
+  fi
+  sshHomePath="$HOME/.ssh/"
+  flagForce=0
+  servers=
+  keyType=ed25519
+  keyBits=2048
 
-	while [ $# != 0 ]; do
-		case $1 in
-		--type)
-			shift
-			case $1 in
-			ed25519 | rsa | dsa)
-				keyType=$1
-				;;
-			*)
-				consoleError "$errorArgument" "Key type $1 is not known: ed25519 | rsa | dsa" 1>&2
-				return $errorArgument
-				;;
-			esac
-			;;
-		--bits)
-			shift
-			minBits=512
-			if [ "$(("$1" + 0))" -lt "$minBits" ]; then
-				consoleError "Key bits is too $minBits: $1 -> $(("$1" + 0))" 1>&2
-				return $errorArgument
-			fi
-			;;
-		--help)
-			usage 0
-			;;
-		--force)
-			flagForce=1
-			;;
-		*)
-			servers="$servers $1"
-			;;
-		esac
-		shift
-	done
+  while [ $# != 0 ]; do
+    case $1 in
+      --type)
+        shift
+        case $1 in
+          ed25519 | rsa | dsa)
+            keyType=$1
+            ;;
+          *)
+            consoleError "$errorArgument" "Key type $1 is not known: ed25519 | rsa | dsa" 1>&2
+            return $errorArgument
+            ;;
+        esac
+        ;;
+      --bits)
+        shift
+        minBits=512
+        if [ "$(("$1" + 0))" -lt "$minBits" ]; then
+          consoleError "Key bits is too $minBits: $1 -> $(("$1" + 0))" 1>&2
+          return $errorArgument
+        fi
+        ;;
+      --help)
+        usage 0
+        ;;
+      --force)
+        flagForce=1
+        ;;
+      *)
+        servers="$servers $1"
+        ;;
+    esac
+    shift
+  done
 
-	if [ ! -d "$sshHomePath" ] && ! mkdir -p .ssh/; then
-		consoleError "Can't create $sshHomePath" 1>&2
-		return 1
-	fi
-	chmod 700 "$sshHomePath"
-	cd "$sshHomePath"
-	user="$(whoami)"
-	keyname="$user@$(uname -n)"
-	if [ $flagForce = 0 ] && [ -f "$keyname" ]; then
-		if [ "$servers" = "" ]; then
-			echo "Key $keyname already exists, exiting." 1>&2
-			exit $errorArgument
-		fi
-	else
-		echo "Generating $keyname (keyType $keyType $keyBits keyBits)"
-		ssh-keygen -f "$keyname" -t "$keyType" -b $keyBits -C "$keyname" -q -N ""
-		cp "$keyname" id_"$keyType"
-		cp "$keyname".pub id_"$keyType".pub
-	fi
-	for s in $servers; do
-		echo "Uploading key and modifying authorized_keys with $s"
-		echo "(Please enter password twice)"
-		echo "cd .ssh
-		put $keyname.pub
+  if [ ! -d "$sshHomePath" ] && ! mkdir -p .ssh/; then
+    consoleError "Can't create $sshHomePath" 1>&2
+    return 1
+  fi
+  chmod 700 "$sshHomePath"
+  cd "$sshHomePath"
+  user="$(whoami)"
+  keyName="$user@$(uname -n)"
+  if [ $flagForce = 0 ] && [ -f "$keyName" ]; then
+    if [ "$servers" = "" ]; then
+      echo "Key $keyName already exists, exiting." 1>&2
+      exit $errorArgument
+    fi
+  else
+    echo "Generating $keyName (keyType $keyType $keyBits keyBits)"
+    ssh-keygen -f "$keyName" -t "$keyType" -b $keyBits -C "$keyName" -q -N ""
+    cp "$keyName" id_"$keyType"
+    cp "$keyName".pub id_"$keyType".pub
+  fi
+  for s in $servers; do
+    echo "Uploading key and modifying authorized_keys with $s"
+    echo "(Please enter password twice)"
+    echo "cd .ssh
+		put $keyName.pub
 		quit" | sftp "$s" >/dev/null
-		echo "cd ~\
+    echo "cd ~\
 		cd .ssh\
 		cat *pub > authorized_keys\
 		exit" | ssh -T "$s" >/dev/null
-	done
+  done
 
 }
 
+generateSSHKeyPair() {
+  local keyName keyType keyBits
+
+  keyType=ed25519
+  keyBits=2048
+  keyName="$1"
+  ssh-keygen -f "$keyName" -t "$keyType" -b $keyBits -C "$keyName" -q -N ""
+}
 #
 # usage [ exitcode ]
 #
 usage() {
-	echo
-	echo "$ME"": Add .ssh key for current user: $(whoami)"
-	echo
-	echo "$ME" [ --force ] [ server ... ]
-	echo
-	echo --force Force the program to create a new key if one exists
-	echo server Servers to connect to to set up authorization
-	echo You will need the password for this server for the current user.
-	echo
+  echo
+  echo "$ME"": Add .ssh key for current user: $(whoami)"
+  echo
+  echo "$ME" [ --force ] [ server ... ]
+  echo
+  echo --force Force the program to create a new key if one exists
+  echo server Servers to connect to to set up authorization
+  echo You will need the password for this server for the current user.
+  echo
 
-	exit "$1"
+  exit "$1"
 }
