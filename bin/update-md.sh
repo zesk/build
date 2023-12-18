b76e4427caa474689abab2b2b839c737edf8e60f
Kent Davidson <kent@marketacumen.com>
2023-12-17 22:24:40 -0500

    update md update

--- bin/update-md.sh
+++ bin/update-md.sh
@@ -10,23 +10,15 @@ errorArgument=2
 
 set -eou pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
+me="$(dirname "$0")"
 
 # shellcheck source=/dev/null
 . ./bin/build/tools.sh
 
-flagSkipCommit=
-while [ $# -gt 0 ]; do
-    case $1 in
-    --skip-commit)
-        flagSkipCommit=1
-        statusMessage consoleWarning "Skipping commit ..."
-        ;;
-    *)
-        _usageUpdateMarkdown $errorArgument "Bad argument $1"
-        ;;
-    esac
-    shift
-done
+_usageUpdateMarkdown() {
+    usageDocument "bin/$me" updateMarkdown "$@"
+}
+
 addNoteTo() {
     statusMessage consoleInfo "Adding note to $1"
     cp "$1" bin/build
@@ -34,34 +26,55 @@ addNoteTo() {
     git add "bin/build/$1"
 }
 
-addNoteTo README.md
-addNoteTo LICENSE.md
-
-buildMarker=bin/build/build.json
-
-statusMessage consoleInfo "Generating build.json"
-printf "%s" "{}" | jq --arg version "$(runHook version-current)" \
-    --arg tag "$(runHook application-tag)" \
-    --arg checksum "$(runHook application-checksum)" \
-    '. + {version: $version, tag: $tag, checksum: $checksum}' >"$buildMarker"
-git add "$buildMarker"
-
 #
-# Disable this to see what environment shows up in commit hooks for GIT*=
+# Usage: {fn} [ --skip-commit ]
+# Argument: --skip-commit - Skip the commit if the files change
 #
-# env | sort >.update-md.env
-#
+updateMarkdown() {
+    flagSkipCommit=
+    while [ $# -gt 0 ]; do
+        case $1 in
+        --skip-commit)
+            flagSkipCommit=1
+            statusMessage consoleWarning "Skipping commit ..."
+            ;;
+        *)
+            _usageUpdateMarkdown $errorArgument "Bad argument $1"
+            ;;
+        esac
+        shift
+    done
+    addNoteTo README.md
+    addNoteTo LICENSE.md
 
-# Do this as long as we are not in the hook
-if ! test $flagSkipCommit; then
-    if ! gitInsideHook; then
-        if gitRepositoryChanged; then
-            statusMessage consoleInfo "Committing build.json"
-            git commit -m "Updating build.json" "$buildMarker"
-            git push origin
+    buildMarker=bin/build/build.json
+
+    statusMessage consoleInfo "Generating build.json"
+    printf "%s" "{}" | jq --arg version "$(runHook version-current)" \
+        --arg tag "$(runHook application-tag)" \
+        --arg checksum "$(runHook application-checksum)" \
+        '. + {version: $version, tag: $tag, checksum: $checksum}' >"$buildMarker"
+    git add "$buildMarker"
+
+    #
+    # Disable this to see what environment shows up in commit hooks for GIT*=
+    #
+    # env | sort >.update-md.env
+    #
+
+    # Do this as long as we are not in the hook
+    if ! test $flagSkipCommit; then
+        if ! gitInsideHook; then
+            if gitRepositoryChanged; then
+                statusMessage consoleInfo "Committing build.json"
+                git commit -m "Updating build.json" "$buildMarker"
+                git push origin
+            fi
+        else
+            statusMessage consoleWarning "Skipping update during commit hook"
         fi
-    else
-        statusMessage consoleWarning "Skipping update during commit hook"
     fi
-fi
-clearLine
+    clearLine
+}
+
+updateMarkdown "$@"
