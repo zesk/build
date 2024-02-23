2fe997ce1906268796dba68603474cc1070015e1
Kent Davidson <kent@marketacumen.com>
2024-02-23 16:10:56 -0500

    fixing error maybe

--- bin/build/tools/console.sh
+++ bin/build/tools/console.sh
@@ -43,27 +43,23 @@ consoleGetColor() {
   done
   if ! sttyOld=$(stty -g 2>/dev/null); then
     noTTY=true
-    # 3 is a copy of stdin
-    exec 3<&0
     # 4 is a copy of stdout
     exec 4>&1
   else
     if ! stty raw -echo min 0 time 0; then
       _consoleGetColor "$errorEnvironment" "stty raw failed" || return $?
     fi
-    # 3 is a redirect to file /dev/tty
-    exec 3</dev/tty
-    # 4 is also a redirect to file /dev/tty
+    # 4 is a redirect to file /dev/tty
     exec 4>/dev/tty
   fi
 
-  # term needs the sleep (or "time 1", but that is 1/10th second).
   if ! printf "\e]%d;?\e\\" "${xtermCode}" >&4; then
     _consoleGetColor "$errorEnvironment" "tty message failed" || return $?
   fi
+  # term needs the sleep (or "time 1", but that is 1/10th second).
   sleep 0.0001 || :
   colors=()
-  if ! read -t 2 -r result <&3; then
+  if ! read -t 2 -r result; then
     success=true
     # remove escape chars
     result="${result#*;}"
