695f32f75870dd126128acb2435bba7b6a813b3b
Kent Davidson <kent@marketacumen.com>
2024-06-30 18:47:34 -0400

    Merging staging and main with release/0.10

--- test/tools/sugar-tests.sh
+++ test/tools/sugar-tests.sh
@@ -8,9 +8,53 @@
 #
 declare -a tests
 
+_wasRun() {
+  local exitCode
+
+  export SUGAR_FILE
+  exitCode=0
+  if [ -f "$SUGAR_FILE" ]; then
+    if [ ! -s "$SUGAR_FILE" ]; then
+      # Zero sized
+      exitCode=98
+    else
+      # Non-zero sized
+      exitCode=99
+    fi
+
+  fi
+  date >>"$SUGAR_FILE"
+  return "$exitCode"
+}
+
 tests+=(testSugar)
 testSugar() {
   local code
+
+  # __return running stuff
+  export SUGAR_FILE
+  SUGAR_FILE=$(mktemp) || _environment mktemp || return $?
+  assertFileExists "$SUGAR_FILE" || return $?
+  code=98
+  assertExitCode --dump --stderr-match "$code" "$code" __return _wasRun || return $?
+  assertFileExists "$SUGAR_FILE" || return $?
+  code=99
+  assertExitCode --dump --stderr-match "$code" "$code" __return _wasRun || return $?
+  assertFileExists "$SUGAR_FILE" || return $?
+  assertEquals $(($(wc -l <"$SUGAR_FILE") + 0)) 2 || return $?
+  __environment rm -rf "$SUGAR_FILE" || return $?
+  assertFileDoesNotExist "$SUGAR_FILE" || return $?
+  code=0
+  assertExitCode 0 __return _wasRun || return $?
+  assertFileExists "$SUGAR_FILE" || return $?
+  code=99
+  assertExitCode --dump --stderr-match "$code" "$code" __return _wasRun || return $?
+  assertFileExists "$SUGAR_FILE" || return $?
+  code=99
+  assertExitCode --dump --stderr-match "$code" "$code" __return _wasRun || return $?
+  assertEquals $(($(wc -l <"$SUGAR_FILE") + 0)) 3 || return $?
+  rm -rf "$SUGAR_FILE"
+  unset SUGAR_FILE
 
   # set -x
 
@@ -27,6 +71,7 @@ testSugar() {
   for code in $(seq 0 127); do
     assertExitCode --stderr-ok "$code" __return _return "$code" || return $?
   done
+
   # _environment
   # _argument
   assertExitCode --stderr-ok 1 _environment || return $?
