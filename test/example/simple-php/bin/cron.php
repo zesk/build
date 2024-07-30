d0c4491c5d24a5ab3d878c9afa34f157b77c4d49
Kent Davidson <kent@marketacumen.com>
2024-07-30 10:39:48 -0400

    Merging staging and main with release/0.11

--- test/example/simple-php/bin/cron.php
+++ test/example/simple-php/bin/cron.php
@@ -1,6 +1,8 @@
 #!/usr/bin/env php
 <?php
-
+/*
+ * Copyright &copy; 2024 Market Acumen, Inc.
+ */
 use zesk\SimpleApplication\Application;
 
 require_once dirname(__DIR__) . '/simple.application.php';
