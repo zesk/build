e7d96173b9acce6d39d42d576584a7b4e23d2d92
Kent Davidson <kent@marketacumen.com>
2024-10-10 09:19:46 -0400

    fixed tests

--- test/tools/iterm2-tests.sh
+++ test/tools/iterm2-tests.sh
@@ -9,7 +9,7 @@ testIterm2() {
   local saved_LC_TERMINAL saved_HOME
 
   saved_HOME=$HOME
-  saved_LC_TERMINAL=${LC_TERMINAL+none}
+  saved_LC_TERMINAL=${LC_TERMINAL-none}
 
   export LC_TERMINAL HOME ITERM2_SHELL_INTEGRATION_WORKS
 
@@ -20,7 +20,7 @@ testIterm2() {
   __environment chmod +x "$HOME/.iterm2_shell_integration.bash" || return $?
 
   LC_TERMINAL=wrong
-  assertExitCode --line "$LINENO" 0 iTerm2Init || return $?
+  assertExitCode --leak ITERM2_SHELL_INTEGRATION_WORKS --line "$LINENO" 0 iTerm2Init || return $?
   assertEquals --line "$LINENO" "$ITERM2_SHELL_INTEGRATION_WORKS" "false" || return $?
   assertNotExitCode --line "$LINENO" 0 isiTerm2 || return $?
   assertNotExitCode --line "$LINENO" --stderr-match "does not support" 0 iTerm2Badge "foo" || return $?
@@ -28,7 +28,7 @@ testIterm2() {
   LC_TERMINAL=iTerm2
   assertExitCode --line "$LINENO" 0 isiTerm2 || return $?
   assertExitCode --dump-binary --line "$LINENO" --stdout-match "Zm9v" 0 iTerm2Badge "foo" </dev/null || return $?
-  assertExitCode --line "$LINENO" 0 iTerm2Init || return $?
+  assertExitCode --leak ITERM2_SHELL_INTEGRATION_WORKS --line "$LINENO" 0 iTerm2Init || return $?
   assertEquals --line "$LINENO" "$ITERM2_SHELL_INTEGRATION_WORKS" "true" || return $?
 
   if [ "$saved_LC_TERMINAL" = "none" ]; then
