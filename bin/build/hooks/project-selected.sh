ca6b7f77fb3b6ca7a54484658d9ca147066c4080
Kent Davidson <kent@marketacumen.com>
2024-10-15 14:14:50 -0400

    bin/install

--- bin/build/hooks/project-selected.sh
+++ bin/build/hooks/project-selected.sh
@@ -17,6 +17,7 @@ source "${BASH_SOURCE[0]%/*}/../tools.sh"
 # Argument: oldHomeDirectory - The old home directory of the project
 __hookProjectSelected() {
   local usage="_${FUNCNAME[0]}" home oldName="" oldHome tools="bin/build/tools.sh"
+  local symbol="🍎"
 
   oldHome=$(usageArgumentString "$usage" "oldHomeDirectory" "${1-}") || return $?
   oldHome=${oldHome%/} # Strip trailing slash
@@ -28,7 +29,7 @@ __hookProjectSelected() {
   name=$(__usageEnvironment "$usage" buildEnvironmentGet APPLICATION_NAME) || return $?
   home=$(__usageEnvironment "$usage" buildHome) || return $?
   [ -n "$name" ] || name="${home##*/}"
-  printf -- "%s %s %s %s\n" "🌎" "$(consoleSubtle "$oldName")" "➜" "$(consoleInfo "$name")"
+  printf -- "%s %s %s %s\n" "$symbol" "$(consoleSubtle "$oldName")" "➜" "$(consoleInfo "$name")"
 }
 ___hookProjectSelected() {
   usageDocument "${BASH_SOURCE[0]}" "${FUNCNAME[0]#_}" "$@"
--- bin/build/install/mariadb-client.sh
+++ bin/build/install/mariadb-client.sh
@@ -8,4 +8,8 @@
 #
 # Copyright &copy; 2024 Market Acumen, Inc.
 #
+
+# IDENTICAL binBuildInstallDeprecatedWarning 1
+printf "%s\n\n    %s %s\n\n" "bin/install/$(basename "${BASH_SOURCE[0]}") was DEPRECATED 2024-10-15 please use" "bin/build/tools.sh" "$(grep dirname "${BASH_SOURCE[0]}" | grep -v DEPRECATED | awk '{ print $3 }')" 1>&2
+
 "$(dirname "${BASH_SOURCE[0]}")/../tools.sh" mariadbInstall "$@"
