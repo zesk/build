737a0600c2131b0d4b3c99fbd8d2d2462c07658b
Kent Davidson <kent@marketacumen.com>
2024-12-16 12:09:28 -0500

    `prettier` no longer supported as it is a node package

--- test/tools/yarn-tests.sh
+++ test/tools/yarn-tests.sh
@@ -8,6 +8,10 @@
 #
 
 testYarnInstallation() {
+  if whichExists yarn; then
+    decorate warning "Yarn is already installed, skipping"
+    return 0
+  fi
   assertExitCode 0 nodeUninstall || return $?
   __checkFunctionInstallsBinary yarn yarnInstall || return $?
   assertExitCode 0 whichExists node || return $?
