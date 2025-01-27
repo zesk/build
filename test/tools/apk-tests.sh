788281bee71d94e2c5831c1968a2b5ae8a5aed6e
Kent Davidson <kent@marketacumen.com>
2025-01-26 21:15:01 -0500

    testAlpineContainer acting odd in pipeline now

--- test/tools/apk-tests.sh
+++ test/tools/apk-tests.sh
@@ -27,8 +27,12 @@ testIsApkInstalled() {
 
 testAlpineContainer() {
   if whichExists docker; then
-    __echo alpineContainer echo "FOO=\"foo\"" # TODO - parse error: Invalid numeric literal at line 2, column 0 on BitBucket pipelines
-    assertExitCode --line "$LINENO" 0 alpineContainer echo "FOO=\"foo\"" || return $?
+    # __echo alpineContainer echo "FOO=\"foo\"" # TODO - parse error: Invalid numeric literal at line 2, column 0 on BitBucket pipelines
+    # WTF? Where is this coming from?
+    alpineContainer echo "FOO=\"foo\"" || :
+    buildEnvironmentLoad LC_TERMINAL TERM || :
+    dockerLocalContainer --image alpine:latest --path /root/build --env LC_TERMINAL="$LC_TERMINAL" --env TERM="$TERM" /root/build/bin/build/need-bash.sh Alpine apk add bash ncurses -- || :
+    assertExitCode --stderr-ok --line "$LINENO" 0 alpineContainer echo "FOO=\"foo\"" || return $?
     assertEquals --line "$LINENO" "$(alpineContainer echo "FOO=\"foo\"")" "FOO=\"foo\"" || return $?
   fi
 }
