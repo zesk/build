072a95811e70afa34cec3fbab6a17ea1a3b13169
Kent Davidson <kent@marketacumen.com>
2025-02-02 19:16:19 -0500

    test fix

--- bin/build/env/XDG_CACHE_HOME.sh
+++ bin/build/env/XDG_CACHE_HOME.sh
@@ -4,5 +4,7 @@
 # Category: Configuration
 # Type: Directory
 # See: https://specifications.freedesktop.org/basedir-spec/latest/
+# shellcheck source=/dev/null
+source "${BASH_SOURCE[0]%/*}/HOME.sh"
 export XDG_CACHE_HOME
-XDG_CACHE_HOME=${XDG_DATA_HOME-"$HOME/.cache"}
+XDG_CACHE_HOME=${XDG_DATA_HOME-"${HOME-}/.cache"}
