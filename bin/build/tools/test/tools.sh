7aab0b97c52b1483a98a5d8f7d9ef791443d238f
Kent Davidson <kent@marketacumen.com>
2025-03-02 01:15:43 -0500

    fail fix

--- bin/build/tools/test/tools.sh
+++ bin/build/tools/test/tools.sh
@@ -811,7 +811,7 @@ __testRun() {
     printf "%s %s %s ...\n" "$(decorate code "$__test")" "$(decorate success "passed")" "$timingText"
   else
     printf "[%d] %s %s %s\n" "$resultCode" "$(decorate code "$__test")" "$(decorate error "FAILED")" "$timingText" 1>&2
-    buildFailed --tail "$quietLog" || :
+    buildFailed "$quietLog" || :
     __TEST_SUITE_RESULT="test $__test failed"
     stickyCode=$errorTest
   fi
