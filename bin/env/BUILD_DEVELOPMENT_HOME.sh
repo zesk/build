13108efafe93539d447d73c5b482a2f232e7b80f
Kent Davidson <kent@marketacumen.com>
2025-04-01 22:18:33 -0400

    Fixing type of `XDEBUG_ENABLED` to `Boolean`

--- bin/env/BUILD_DEVELOPMENT_HOME.sh
+++ bin/env/BUILD_DEVELOPMENT_HOME.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Copyright &copy; 2025 Market Acumen, Inc.
 # Type: String
+# Category: Development
 # All about BUILD_DEVELOPMENT_HOME and how it is used
 export BUILD_DEVELOPMENT_HOME
 BUILD_DEVELOPMENT_HOME="${BUILD_DEVELOPMENT_HOME-}"
