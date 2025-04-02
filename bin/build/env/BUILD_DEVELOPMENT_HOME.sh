926e11324dc81fd90d3a2bfeaba326ee2b9d5c51
Kent Davidson <kent@marketacumen.com>
2025-04-01 22:19:47 -0400

    Fixing type of `XDEBUG_ENABLED` to `Boolean`

--- bin/build/env/BUILD_DEVELOPMENT_HOME.sh
+++ bin/build/env/BUILD_DEVELOPMENT_HOME.sh
@@ -2,6 +2,7 @@
 # Copyright &copy; 2025 Market Acumen, Inc.
 # Type: String
 # Category: Development
-# All about BUILD_DEVELOPMENT_HOME and how it is used
+# Directory where Zesk Build is being developed in the file system (for other projects to test against a changed version)
+# See: buildDevelopmentLink
 export BUILD_DEVELOPMENT_HOME
 BUILD_DEVELOPMENT_HOME="${BUILD_DEVELOPMENT_HOME-}"
