cb2dcb660057da2c85fa7fc7c5db6070e4646886
Kent Davidson <kent@marketacumen.com>
2025-04-05 12:43:23 -0400

    Added `application.sh` to core to easier incorporate Zesk Build in other projects

--- bin/build/env/BUILD_DOCKER_PLATFORM.sh
+++ bin/build/env/BUILD_DOCKER_PLATFORM.sh
@@ -27,10 +27,3 @@
 # Type: String
 export BUILD_DOCKER_PLATFORM
 BUILD_DOCKER_PLATFORM=${BUILD_DOCKER_PLATFORM-}
-
-# ~/marketruler/devops 👉 docker buildx ls
-# NAME/NODE       DRIVER/ENDPOINT STATUS  BUILDKIT             PLATFORMS
-# default         docker
-#   default       default         running v0.11.7+d3e6c1360f6e linux/arm64, linux/amd64, linux/amd64/v2, linux/riscv64, linux/ppc64le, linux/s390x, linux/386, linux/mips64le, linux/mips64, linux/arm/v7, linux/arm/v6
-# desktop-linux * docker
-#   desktop-linux desktop-linux   running v0.11.7+d3e6c1360f6e linux/arm64, linux/amd64, linux/amd64/v2, linux/riscv64, linux/ppc64le, linux/s390x, linux/386, linux/mips64le, linux/mips64, linux/arm/v7, linux/arm/v6
