1520163c75c1d807cd0a32f74452b16bb836f2d5
Kent Davidson <kent@marketacumen.com>
2025-05-09 19:50:51 -0400

    Added support for `pcre2` installations and `__pcregrep` call in platform

--- bin/build/env/BUILD_PACKAGE_MANAGER.sh
+++ bin/build/env/BUILD_PACKAGE_MANAGER.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Copyright &copy; 2025 Market Acumen, Inc.
 # Type: Executable
+# Category: Installation
 # The default package manager on systems which have more than one package manager available.
 export BUILD_PACKAGE_MANAGER
 BUILD_PACKAGE_MANAGER="${BUILD_PACKAGE_MANAGER-}"
