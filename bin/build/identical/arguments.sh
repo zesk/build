60e492150802d5731a250c284696ec09431a2f87
Kent Davidson <kent@marketacumen.com>
2025-05-09 18:27:52 -0400

    Added support for `pcre2` installations and `__pcregrep` call in platform

--- bin/build/identical/arguments.sh
+++ bin/build/identical/arguments.sh
@@ -10,6 +10,8 @@
 ___documentTemplateFunction() {
   local usage="_${FUNCNAME[0]}"
 
+  local manager=""
+
   # _IDENTICAL_ argument-case-blank-argument-header 4
   local __saved=("$@") __count=$#
   while [ $# -gt 0 ]; do
@@ -20,6 +22,12 @@ ___documentTemplateFunction() {
       "$usage" 0
       return $?
       ;;
+    # IDENTICAL managerArgumentHandler 5
+    --manager)
+      shift
+      manager=$(usageArgumentString "$usage" "$argument" "${1-}")
+      packageManagerValid "$manager" || __throwArgument "$usage" "Manager is invalid: $(decorate code "$manager")" || return $?
+      ;;
     *)
       # _IDENTICAL_ argumentUnknown 1
       __throwArgument "$usage" "unknown #$__index/$__count \"$argument\" ($(decorate each code "${__saved[@]}"))" || return $?
@@ -28,6 +36,10 @@ ___documentTemplateFunction() {
     # _IDENTICAL_ argument-esac-shift 1
     shift
   done
+
+  # IDENTICAL managerArgumentValidation 2
+  [ -n "$manager" ] || manager=$(packageManagerDefault) || __throwEnvironment "$usage" "No package manager" || return $?
+  whichExists "$manager" || __throwEnvironment "$usage" "$manager does not exist" || return $?
 
   return 0
 }
