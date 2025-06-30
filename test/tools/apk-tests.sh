bd8eca7bdb7da18757b8a3cf0626a5d7acedf6be
Kent Davidson <kent@marketacumen.com>
2025-06-30 12:08:31 -0400

    fixing alpine test

--- test/tools/apk-tests.sh
+++ test/tools/apk-tests.sh
@@ -37,12 +37,23 @@ testIsApkInstalled() {
 
 testAlpineContainer() {
   if whichExists docker; then
+
+    local usage="_return" home
+
+    home=$(__catchEnvironment "$usage" buildHome) || return $?
+    __catchEnvironment "$usage" muzzle pushd "$home" || return $?
+
+    # In BitBucket pipelines, only location you can run Alpine volume shares are within the initial build directory
+
     assertExitCode 0 alpineContainer echo "FOO=\"foo\"" || return $?
     local value
 
     value=$(trimSpace "$(alpineContainer echo "FOO=\"foo\"")")
 
     assertEquals "$value" "FOO=\"foo\"" || return $?
+
+    __catchEnvironment "$usage" muzzle popd || return $?
+
   fi
   return 0
 }
