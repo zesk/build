170682f59971aa5b78d824eefb50ce1e20ed3077
Kent Davidson <kent@marketacumen.com>
2025-07-01 22:56:01 -0400

    Adding Darwin install notes

--- bin/build/tools/mariadb.sh
+++ bin/build/tools/mariadb.sh
@@ -211,3 +211,35 @@ _mariadbConnect() {
 #
 # You can start the MariaDB daemon with:
 # cd '/opt/local' ; /opt/local/lib/mariadb-11.4/bin/mariadbd-safe --datadir='/opt/local/var/db/mariadb-11.4'
+
+#
+#  Installing MariaDB/MySQL system tables in '/opt/local/var/db/mariadb-11.4' ...
+#  OK
+#
+#  To start mariadbd at boot time you have to copy
+#  support-files/mariadb.service to the right place for your system
+#
+#
+#  Two all-privilege accounts were created.
+#  One is root@localhost, it has no password, but you need to
+#  be system 'root' user to connect. Use, for example, sudo mysql
+#  The second is _mysql@localhost, it has no password either, but
+#  you need to be the system '_mysql' user to connect.
+#  After connecting you can set the password, if you would need to be
+#  able to connect as any of these users with a password and without sudo
+#
+#  See the MariaDB Knowledgebase at https://mariadb.com/kb
+#
+#  You can start the MariaDB daemon with:
+#  cd '/opt/local' ; /opt/local/lib/mariadb-11.4/bin/mariadbd-safe --datadir='/opt/local/var/db/mariadb-11.4'
+#
+#  You can test the MariaDB daemon with mariadb-test-run.pl
+#  cd '/opt/local/share/mariadb-11.4/mysql-test' ; perl mariadb-test-run.pl
+#
+#  Please report any problems at https://mariadb.org/jira
+#
+#  The latest information about MariaDB is available at https://mariadb.org/.
+#
+#  Consider joining MariaDB's strong and vibrant community:
+#  https://mariadb.org/get-involved/
+#
