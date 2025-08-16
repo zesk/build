dc332c120f6e6ac6d4379e90be22d03eb2415eba
Kent Davidson <kent@marketacumen.com>
2025-08-16 10:57:07 -0400

    fixed leak in `testDockerComposeIsRunning`

--- test/tools/docker-compose-tests.sh
+++ test/tools/docker-compose-tests.sh
@@ -5,28 +5,32 @@
 # Copyright &copy; 2025 Market Acumen, Inc.
 #
 
+# Tag: docker
 testIsDockerComposeRunning() {
-  local usage="_return"
+  local handler="_return"
 
   if whichExists docker; then
     local oldHome
 
-    oldHome=$(__catch "$usage" buildHome) || return $?
+    oldHome=$(__catch "$handler" buildHome) || return $?
 
     __mockValue BUILD_HOME
 
     export BUILD_HOME
 
-    BUILD_HOME=$(fileTemporaryName "$usage" -d) || return $?
+    local newHome
+    newHome=$(fileTemporaryName "$handler" -d) || return $?
 
-    __catchEnvironment "$usage" mkdir "$BUILD_HOME/bin" || return $?
-    __catchEnvironment "$usage" cp -R "$oldHome/bin/build" "$BUILD_HOME/bin/build" || return $?
-    __catchEnvironment "$usage" pushd "$BUILD_HOME" || return $?
+    BUILD_HOME=$newHome
+    __catchEnvironment "$handler" mkdir "$BUILD_HOME/bin" || return $?
+    __catchEnvironment "$handler" cp -R "$oldHome/bin/build" "$BUILD_HOME/bin/build" || return $?
+    __catchEnvironment "$handler" pushd "$BUILD_HOME" || return $?
 
     assertNotExitCode --stderr-match "Missing" --stderr-match ".STAGING.env" 0 dockerComposeIsRunning || return $?
-    __catchEnvironment "$usage" touch "$BUILD_HOME/.STAGING.env" || return $?
+    __catchEnvironment "$handler" touch "$BUILD_HOME/.STAGING.env" || return $?
     assertNotExitCode --stderr-match "Missing" --stderr-match "docker-compose.yml" 0 dockerComposeIsRunning || return $?
 
+    __catchEnvironment "$handler" rm -rf "$newHome" || return $?
     __mockValueStop BUILD_HOME
   fi
 }
