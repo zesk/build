f37e2b99a74d5bfe48d54d6bd9a3c2d54110baab
Kent Davidson <kent@marketacumen.com>
2025-09-15 10:55:35 -0400

    Fixed `reloadChanges` which appeared to be triggering every invokation for multiple directory configurations.

--- bin/build/tools/prompt/modules/reload-changes/module.sh
+++ bin/build/tools/prompt/modules/reload-changes/module.sh
@@ -23,96 +23,111 @@ __bashPromptModule_reloadChanges() {
   ! $debug || decorate info "reloadChanges:"
   ! $debug || decorate pair cacheFile "$(decorate file "$cacheFile")"
 
-  local name="" source=""
-  local reloadSource=false
+  local name="" source="" directories=()
+  local sourceIndex=0
+
   while read -r argument; do
     if [ -z "$source" ]; then
       source=$(usageArgumentString "$handler" "source" "$argument") || return $?
       [ "${source:0:1}" = "/" ] || source="$home/$source"
       ! $debug || decorate pair source "$(decorate file "$source")"
+      pathStateFile="$(__reloadChangesCacheFile "$handler" "$sourceIndex")" || return $?
+      sourceIndex=$((sourceIndex + 1))
       continue
     elif [ -z "$name" ]; then
       name=$(usageArgumentString "$handler" "name" "$argument") || return $?
       ! $debug || decorate pair name "$name"
       continue
     elif [ "$argument" = "--" ]; then
-      reloadSource=false
-      name=""
+      if ! fileDirectoryExists "$source"; then
+        decorate warning "$source has moved"
+        removeSources+=("$source")
+      else
+        if ___bashPromptModule_reloadChangesCheck "$handler" "$debug" "$pathStateFile" "$source" "$name" "${directories[@]}"; then
+          # shellcheck source=/dev/null
+          source "$source"
+        fi
+      fi
       source=""
-      continue
-    fi
-
-    local fileNewest path pathStateFile modified=0 filename="" prefix=""
-
-    path=$(usageArgumentString "$handler" "path" "$argument") || return $?
-
-    # Reloaded on a prior path, just skip until the next record
-    if $reloadSource; then
-      ! $debug || decorate pair Skipping path "$(decorate file "$path")"
-      continue
-    fi
-
-    ! $debug || decorate pair path "$(decorate file "$path")"
-
-    if [ ! -d "$path" ]; then
-      decorate warning "$path was removed"
-      removeSources+=("$source")
-      continue
-    fi
-    if ! fileDirectoryExists "$source"; then
-      decorate warning "$source has moved"
-      removeSources+=("$source")
-      continue
-    fi
-
-    pathStateFile="$(__reloadChangesCacheFile "$handler" "$pathIndex")" || return $?
-    if [ -f "$pathStateFile" ]; then
-      modified="$(head -n 1 "$pathStateFile")"
-      filename="$(tail -n 1 "$pathStateFile")"
-      if ! isInteger "$modified"; then
-        if $debug; then
-          decorate warning "Modified in $pathStateFile is non-integer: \"$modified\""
-        else
-          __catchEnvironment "$handler" rm -f "$pathStateFile" || return $?
-        fi
-        modified=0
-      elif isInteger "${__BASH_PROMPT_RELOAD_CHANGES-}"; then
-        if [ "$modified" -gt "${__BASH_PROMPT_RELOAD_CHANGES}" ]; then
-          ! $debug || decorate info "Another process detected changes in $(decorate label "$name") [$modified]"
-          __BASH_PROMPT_RELOAD_CHANGES="$modified"
-          reloadSource=true
-        fi
-      fi
-    fi
-
-    [ "${path:0:1}" = "/" ] || path="$home/$path"
-    fileNewest=$(directoryNewestFile "$path" --find -name '*.sh') || return $?
-    newestModified=$(fileModificationTime "$fileNewest") || return $?
-    ! $debug || decorate pair fileNewest "$(decorate file "$fileNewest")"
-    if [ "$newestModified" -ne "$modified" ] || [ "$fileNewest" != "$filename" ]; then
-      ! $debug || decorate info "$newestModified ($fileNewest) -gt $modified ($filename)"
-      prefix=""
-      [ -z "$filename" ] || prefix="$(decorate file "$filename") -> "
-      [ "$filename" != "$fileNewest" ] || prefix="✏️"
-      printf "%s %s\n" "$(decorate value "$name")" "$(decorate info "code changed, reloading $(decorate file "$source") [$prefix$(decorate file "$fileNewest")]")"
-      ! $debug || decorate info "Saving new state file $newestModified $fileNewest"
-      printf "%s\n" "$newestModified" "$fileNewest" >"$pathStateFile"
-      if isInteger "${__BASH_PROMPT_RELOAD_CHANGES-}" && [ "$__BASH_PROMPT_RELOAD_CHANGES" -lt "$newestModified" ]; then
-        __BASH_PROMPT_RELOAD_CHANGES="$newestModified"
-      fi
-      reloadSource=true
+      name=""
+      directories=()
     else
-      ! $debug || decorate error "! $newestModified -eq $modified"
-      continue
+      [ -n "$argument" ] || continue
+      if [ ! -d "$argument" ]; then
+        decorate warning "$argument was removed"
+        removeSources+=("$source")
+      else
+        directories+=("$argument")
+      fi
     fi
-
-    if $reloadSource; then
-      # shellcheck source=/dev/null
-      source "$source"
-    fi
-    pathIndex=$((pathIndex + 1))
   done <"$cacheFile"
+
   for name in "${removeSources[@]+"${removeSources[@]+}"}"; do
     __reloadChangesRemove "$handler" "$cacheFile" "$source" || return $?
   done
+
+}
+
+___bashPromptModule_reloadChangesCheck() {
+  local handler="$1" && shift
+  local debug="$1" && shift
+  local pathStateFile="$1" && shift
+  local source="$1" && shift
+  local name="$1" && shift
+
+  local maxModified=0 maxNewestFile=""
+  while [ $# -gt 0 ]; do
+    local path="$1" fileNewest newestModified
+
+    ! $debug || decorate pair path "$(decorate file "$path") (#$pathIndex)"
+
+    fileNewest=$(directoryNewestFile "$path" --find -name '*.sh') || return $?
+    newestModified=$(fileModificationTime "$fileNewest") || return $?
+
+    if [ "$maxModified" -eq 0 ] || [ "$newestModified" -gt "$maxModified" ]; then
+      maxModified=$newestModified
+      maxNewestFile=$fileNewest
+    fi
+    shift
+  done
+  local stateModified=0 stateNewestFile=""
+
+  if [ -f "$pathStateFile" ]; then
+    stateModified="$(head -n 1 "$pathStateFile")"
+    stateNewestFile="$(tail -n 1 "$pathStateFile")"
+    if ! isInteger "$stateModified"; then
+      if $debug; then
+        decorate warning "Modified in $pathStateFile is non-integer: \"$stateModified\""
+      else
+        __catchEnvironment "$handler" rm -f "$pathStateFile" || return $?
+      fi
+      stateModified=0
+    elif isInteger "${__BASH_PROMPT_RELOAD_CHANGES-}"; then
+      if [ "$stateModified" -gt "${__BASH_PROMPT_RELOAD_CHANGES}" ]; then
+        ! $debug || decorate info "Another process detected changes in $(decorate label "$name") [$stateModified]"
+        __BASH_PROMPT_RELOAD_CHANGES="$stateModified"
+        return 0
+      fi
+    fi
+  fi
+
+  if [ "$maxModified" -eq "$stateModified" ] && [ "$maxNewestFile" = "$stateNewestFile" ]; then
+    ! $debug || decorate success "No changes: $stateModified $stateNewestFile"
+    return 1
+  fi
+
+  ! $debug || decorate info "$newestModified ($stateNewestFile) -gt $maxModified ($maxNewestFile)"
+
+  local prefix=""
+  [ -z "$stateNewestFile" ] || prefix="$(decorate file "$stateNewestFile") -> "
+  [ "$maxNewestFile" != "$stateNewestFile" ] || prefix="✏️"
+  printf "%s %s\n" "$(decorate value "$name")" "$(decorate info "code changed, reloading $(decorate file "$source") [$prefix$(decorate file "$maxNewestFile")]")"
+  ! $debug || decorate info "Saving new state file $maxModified $maxNewestFile"
+
+  printf "%s\n" "$maxModified" "$maxNewestFile" >"$pathStateFile"
+  if isInteger "${__BASH_PROMPT_RELOAD_CHANGES-}" && [ "$__BASH_PROMPT_RELOAD_CHANGES" -lt "$maxModified" ]; then
+    __BASH_PROMPT_RELOAD_CHANGES="$maxModified"
+  fi
+  return 0
+
 }
--- bin/build/tools/prompt/reloadChanges.sh
+++ bin/build/tools/prompt/reloadChanges.sh
@@ -24,3 +24,4 @@ _reloadChanges() {
   # __IDENTICAL__ usageDocument 1
   usageDocument "${BASH_SOURCE[0]}" "${FUNCNAME[0]#_}" "$@"
 }
+
