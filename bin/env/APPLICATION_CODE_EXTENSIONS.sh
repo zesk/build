5631404654310d05db72644ded2e059721fc8915
Kent Davidson <kent@marketacumen.com>
2025-09-19 19:36:40 -0400

    removed YML and JSON from app files

--- bin/env/APPLICATION_CODE_EXTENSIONS.sh
+++ bin/env/APPLICATION_CODE_EXTENSIONS.sh
@@ -4,4 +4,4 @@
 # Category: Application
 # List of extensions for code in the application
 export APPLICATION_CODE_EXTENSIONS
-APPLICATION_CODE_EXTENSIONS="conf:sh:txt:json:yml"
+APPLICATION_CODE_EXTENSIONS="conf:sh:txt"
