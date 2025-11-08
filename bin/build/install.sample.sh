637af5dd6bb1ba8cb5fde4642ab6c16b5a0ec010
Kent Davidson <kent@marketacumen.com>
2025-11-08 14:25:22 -0500

    Fixed `quoteGrepPattern` to support backslashes correctly (PHP work and namespaces, yep.)

--- bin/build/install.sample.sh
+++ bin/build/install.sample.sh
@@ -696,8 +696,8 @@ urlFetch() {
         shift
         break
       else
-        # _IDENTICAL_ argumentUnknownHandler 1
-        throwArgument "$handler" "unknown #$__index/$__count \"$argument\" ($(decorate each code -- "${__saved[@]}"))" || return $?
+      # _IDENTICAL_ argumentUnknownHandler 1
+      throwArgument "$handler" "unknown #$__index/$__count \"$argument\" ($(decorate each code -- "${__saved[@]}"))" || return $?
       fi
       ;;
     esac
