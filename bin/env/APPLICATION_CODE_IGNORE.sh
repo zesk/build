00c23759948e978a2ca839592f718dbfedec66d9
Kent Davidson <kent@marketacumen.com>
2025-11-13 20:28:28 -0500

    `APPLICATION_CODE_IGNORE` testing and added `--debug` testing for `hookRun application-files --debug` if you want

--- bin/env/APPLICATION_CODE_IGNORE.sh
+++ bin/env/APPLICATION_CODE_IGNORE.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Copyright &copy; 2025 Market Acumen, Inc.
-# Type: String
+# Type: ColonDelimitedList
 # Category: Application
-# All about APPLICATION_CODE_IGNORE and how it is used
+# List of paths which are not our code (skipped in hook `application-files`)
 export APPLICATION_CODE_IGNORE
 APPLICATION_CODE_IGNORE="/test/"
