33b7fe30fb7db0aede537171f60b96cf0d2d6965
Kent Davidson <kent@marketacumen.com>
2025-11-14 20:14:44 -0500

    `bitbucket` test added

--- test/tools/bitbucket-tests.sh
+++ test/tools/bitbucket-tests.sh
@@ -4,8 +4,19 @@
 #
 
 testBitbucketPRNewURL() {
+  local handler="returnMessage"
+
   # Simple numbers
   assertExitCode --stderr-match "Org" --stderr-match "required" 2 bitbucketPRNewURL || return $?
   assertExitCode --stderr-match "Repository" --stderr-match "required" 2 bitbucketPRNewURL "company" || return $?
+  assertExitCode --stdout-match "https://" --stdout-match "bitbucket.org" 0 bitbucketPRNewURL --source test "company" "repo" || return $?
+
+  assertNotExitCode --stderr-match "not a git repository" 0 bitbucketPRNewURL "company" "repo" || return $?
+
+  local home
+  home=$(catchReturn "$handler" buildHome) || return $?
+  catchEnvironment "$handler" muzzle pushd "$home" || return $?
   assertExitCode --stdout-match "https://" --stdout-match "bitbucket.org" 0 bitbucketPRNewURL "company" "repo" || return $?
+
+  catchEnvironment "$handler" muzzle popd || return $?
 }
