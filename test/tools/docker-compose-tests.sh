11013c0dc7a4b45b128dd0207fc60e9a074616f1
Kent Davidson <kent@marketacumen.com>
2025-12-13 12:14:23 -0500

    fixing docker-compose tests

--- test/tools/docker-compose-tests.sh
+++ test/tools/docker-compose-tests.sh
@@ -26,9 +26,25 @@ testIsDockerComposeRunning() {
     catchEnvironment "$handler" cp -R "$oldHome/bin/build" "$BUILD_HOME/bin/build" || return $?
     catchEnvironment "$handler" muzzle pushd "$BUILD_HOME" || return $?
 
-    assertNotExitCode --stderr-match "Missing" --stderr-match ".STAGING.env" 0 dockerComposeIsRunning || return $?
-    catchEnvironment "$handler" touch "$BUILD_HOME/.STAGING.env" || return $?
     assertNotExitCode --stderr-match "Missing" --stderr-match "docker-compose.yml" 0 dockerComposeIsRunning || return $?
+    catchEnvironment "$handler" touch "$BUILD_HOME/docker-compose.yml" || return $?
+
+    assertNotExitCode --stderr-match "empty compose file" 0 dockerComposeIsRunning || return $?
+
+    local lines=(
+      "services:"
+      "  web:"
+      "    build:"
+      "      context: ./"
+      "      dockerfile: ./Dockerfile"
+      "    ports:"
+      "      - 4000:80"
+    )
+    catchEnvironment "$handler" printf "%s\n" "${lines[@]}" >"$BUILD_HOME/docker-compose.yml" || return $?
+    dumpPipe docker-compose.yml <"$BUILD_HOME/docker-compose.yml"
+    catchEnvironment "$handler" printf "%s\n" "FROM php:8.3-apache" >"$BUILD_HOME/Dockerfile" || return $?
+
+    assertExitCode 1 dockerComposeIsRunning || return $?
 
     catchEnvironment "$handler" muzzle popd || return $?
     catchEnvironment "$handler" rm -rf "$newHome" || return $?
