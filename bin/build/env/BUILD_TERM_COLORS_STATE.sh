93bca72298debbfbf543f69c871d15126437c78f
Kent Davidson <kent@marketacumen.com>
2026-01-13 14:53:18 -0500

    Added `shfmt` to pre-commit hook for bash (only if installed)

---
+++ bin/build/env/BUILD_TERM_COLORS_STATE.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Copyright &copy; 2026 Market Acumen, Inc.
+# Type: String
+# Category: Application
+# State to store state of current terminal color state
+# See: bashPromptModule_TermColors
+export BUILD_TERM_COLORS_STATE
+BUILD_TERM_COLORS_STATE="${BUILD_TERM_COLORS_STATE-}"
