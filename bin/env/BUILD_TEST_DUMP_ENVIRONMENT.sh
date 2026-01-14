55aebcc3e0f89c04e7b8a7af76cbd79b27193936
Kent Davidson <kent@marketacumen.com>
2026-01-14 18:29:25 -0500

    Removed `backgroundProcess` from core `bin/developer.sh` - need to test better for longer/slower processes

---
+++ bin/env/BUILD_TEST_DUMP_ENVIRONMENT.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Dump environment at each step
+# Copyright &copy; 2026 Market Acumen, Inc.
+# Category: Internal
+# Type: Boolean
+export BUILD_TEST_DUMP_ENVIRONMENT
+BUILD_TEST_DUMP_ENVIRONMENT="${BUILD_TEST_DUMP_ENVIRONMENT-}"
