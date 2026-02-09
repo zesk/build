02b5447b09e8fc0e5dcab77daca83cc16929055e
Kent Davidson <kent@marketacumen.com>
2026-02-09 11:25:25 -0500

    `testSuite` now supports `junit` output as well as running tests after failures

---
+++ bin/build/documentation/trimWords.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Copyright &copy; 2026 Market Acumen, Inc.
+# Generated on 2026-02-09
+# shellcheck disable=SC2034
+argument="wordCount - PositiveInteger. Words to output"$'\n'"word0 ... - EmptyString. One or more words to output"$'\n'""
+base="text.sh"
+description="Remove words from the end of a phrase"$'\n'""
+example="    printf \"%s: %s\\n\" \"Summary:\" \"\$(trimWords 10 \$description)\""$'\n'""
+file="bin/build/tools/text.sh"
+foundNames=([0]="argument" [1]="example" [2]="tested")
+rawComment="Remove words from the end of a phrase"$'\n'"Argument: wordCount - PositiveInteger. Words to output"$'\n'"Argument: word0 ... - EmptyString. One or more words to output"$'\n'"Example:     printf \"%s: %s\\n\" \"Summary:\" \"\$(trimWords 10 \$description)\""$'\n'"Tested: No"$'\n'""$'\n'""
+return_code="0 - Success"$'\n'"1 - Environment error"$'\n'"2 - Argument error"$'\n'""
+sourceFile="bin/build/tools/text.sh"
+sourceHash="c20cbacc0c65838645f2e404358fc35b5b38e188"
+summary="Remove words from the end of a phrase"
+summaryComputed="true"
+tested="No"$'\n'""
+usage="trimWords [ wordCount ] [ word0 ... ]"
+# shellcheck disable=SC2016
+helpConsole=''$'\e''[[(label)]mUsage'$'\e''[0m: '$'\e''[[(info)]mtrimWords'$'\e''[0m '$'\e''[[(blue)]m[ wordCount ]'$'\e''[0m '$'\e''[[(blue)]m[ word0 ... ]'$'\e''[0m'$'\n'''$'\n''    '$'\e''[[(blue)]mwordCount  '$'\e''[[(value)]mPositiveInteger. Words to output'$'\e''[[(reset)]m'$'\n''    '$'\e''[[(blue)]mword0 ...  '$'\e''[[(value)]mEmptyString. One or more words to output'$'\e''[[(reset)]m'$'\n'''$'\n''Remove words from the end of a phrase'$'\n'''$'\n''Return codes:'$'\n''- '$'\e''[[(code)]m0'$'\e''[[(reset)]m - Success'$'\n''- '$'\e''[[(code)]m1'$'\e''[[(reset)]m - Environment error'$'\n''- '$'\e''[[(code)]m2'$'\e''[[(reset)]m - Argument error'$'\n'''$'\n''Example:'$'\n''    printf "%s: %s\n" "Summary:" "$(trimWords 10 $description)"'$'\n'''
+# shellcheck disable=SC2016
+helpPlain='Usage: trimWords [ wordCount ] [ word0 ... ]'$'\n'''$'\n''    wordCount  PositiveInteger. Words to output'$'\n''    word0 ...  EmptyString. One or more words to output'$'\n'''$'\n''Remove words from the end of a phrase'$'\n'''$'\n''Return codes:'$'\n''- 0 - Success'$'\n''- 1 - Environment error'$'\n''- 2 - Argument error'$'\n'''$'\n''Example:'$'\n''    printf "%s: %s\n" "Summary:" "$(trimWords 10 $description)"'$'\n'''
