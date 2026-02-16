ff0aeb233edd245a3d9c319e9bef604f6d9024ae
Kent Davidson <kent@marketacumen.com>
2026-02-16 15:01:54 -0600

    Adding `--index-file` to `buildTestSuite` to speed up tests

--- bin/tools/test.sh
+++ bin/tools/test.sh
@@ -22,21 +22,20 @@ __buildTestRequirements() {
 # See: testSuite
 buildTestSuite() {
   local handler="_${FUNCNAME[0]}"
-  local testHome
 
   ! consoleHasColors || decorateInitialized || muzzle consoleConfigureDecorate || :
 
   __help "$handler" "$@" || return 0
 
-  testHome="$(catchReturn "$handler" buildHome)" || return $?
-  [ -d "$testHome/test" ] || throwArgument "$handler" "Missing test directory" || return $?
+  local home && home=$(catchReturn "$handler" buildHome) || return $?
+  [ -d "$home/test" ] || throwArgument "$handler" "Missing test directory" || return $?
 
   # Include our own test support files if needed
-  [ ! -d "$testHome/test/support" ] || catchEnvironment "$handler" bashSourcePath "$testHome/test/support" || return $?
+  [ ! -d "$home/test/support" ] || catchEnvironment "$handler" bashSourcePath "$home/test/support" || return $?
 
   __buildTestRequirements "$handler" || return $?
 
-  catchEnvironment "$handler" testSuite --cd-away --delete-common --tests "$testHome/test/tools/" --index-file "$home/test/tests.index" "$@" || return $?
+  catchEnvironment "$handler" testSuite --cd-away --delete-common --tests "$home/test/tools/" --index-file "$home/test/tests.index" "$@" || return $?
 }
 _buildTestSuite() {
   name="$(buildEnvironmentGet APPLICATION_NAME)" fn="${FUNCNAME[0]#_}" _testSuite "$@"
