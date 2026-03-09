03e4a144d38c6b72034eab4239843d0d8874ef5c
Kent Davidson <kent@marketacumen.com>
2026-03-09 11:56:46 -0400

    adding analytics to correct spot

--- documentation/source/js/build.js
+++ documentation/source/js/build.js
@@ -2,6 +2,14 @@
  *
  * Copyright &copy; 2026 Market Acumen, Inc.
  */
-(function () {
+(function (w) {
     console.log('build.js active');
-}());
+    if (w.document.URL.indexOf('.zesk.com')) {
+        (function (c, r, x, id, e) {
+            var d = c.document, p = d.getElementsByTagName(x)[0], n = d.createElement(x);
+            (c[r] = c[r] || []).push(e);
+            n.src = '//www.conversionruler.com/bin/js.php?siteid=' + id;
+            p.parentNode.insertBefore(n, p);
+        })(w, '_crq', 'script', 10805, 0)
+    }
+}(window));
