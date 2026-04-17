b4574c9dac5da4c1c845b44eb8e6db9c5fbf39f5
Kent Davidson <kent@marketacumen.com>
2026-04-17 13:21:45 -0400

    Added tracing to `returnMessage`

--- bin/tools/pre-release.sh
+++ bin/tools/pre-release.sh
@@ -43,7 +43,7 @@ buildPreRelease() {
   __buildPreReleaseStep "$handler" "$interruptCode" "Usage remove deprecated" buildFunctionsRemoveDeprecated || exitCode=$?
   [ "$exitCode" != "$interruptCode" ] || return "$interruptCode"
 
-  __buildPreReleaseStep "$handler" "$interruptCode" "Functions compile" buildFunctionsDerivedCompile || exitCode=$?
+  __buildPreReleaseStep "$handler" "$interruptCode" "Functions compile" buildFunctionsDerivedCompile --fingerprint || exitCode=$?
   [ "$exitCode" != "$interruptCode" ] || return "$interruptCode"
 
   __buildPreReleaseStep "$handler" "$interruptCode" "Fingerprint" fingerprint || exitCode=$?
