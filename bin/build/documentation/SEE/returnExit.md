72d5a48a194e243eaab78eda958841bfa64b87ac
Kent Davidson <kent@marketacumen.com>
2026-04-28 22:44:48 -0400

    Added better error handling for `__fileListColumn` and also `fileOwner`

---
+++ bin/build/documentation/SEE/returnExit.md
@@ -0,0 +1,1 @@
+[returnExit]({rel}tools/assert.md#returnexit) - Exit return code"$'\n'" ([source](https://github.com/zesk/build/blob/main/bin/build/tools/test.sh#L1533))
---
+++ bin/build/documentation/returnExit.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# Copyright &copy; 2026 Market Acumen, Inc.
+# Generated on 2026-04-28
+# shellcheck disable=SC2034
+argument="none"
+base="test.sh"
+description="Return code is \`exit\`"$'\n'""
+file="bin/build/tools/test.sh"
+fn="returnExit"
+foundNames=([0]="summary" [1]="return_code")
+line="1533"
+lowerFn="returnexit"
+rawComment="Summary: Exit return code"$'\n'"Return code is \`exit\`"$'\n'"Return Code: 120"$'\n'""$'\n'""
+return_code="120"$'\n'""
+sourceFile="bin/build/tools/test.sh"
+sourceHash="78c7da5cbc1777fd8206d96854e19720ad1957a9"
+sourceLine="1533"
+summary="Exit return code"$'\n'""
+usage="returnExit"
+# shellcheck disable=SC2016
+helpConsole=''$'\e''[[(label)]mUsage'$'\e''[0m: '$'\e''[[(info)]mreturnExit'$'\e''[0m'$'\n'''$'\n''Return code is '$'\e''[[(code)]mexit'$'\e''[[(reset)]m'$'\n'''$'\n''Return codes:'$'\n''- 120'$'\n'''
+# shellcheck disable=SC2016
+helpPlain='Usage: returnExit'$'\n'''$'\n''Return code is exit'$'\n'''$'\n''Return codes:'$'\n''- 120'$'\n'''
+documentationPath="documentation/source/tools/assert.md"
